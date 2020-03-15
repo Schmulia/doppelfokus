9c683eaab087a21c283043cad2ae96cc90a89835
Julia <schmulia@doppelfokus.com>
2020-03-15 21:37:40 +0100

    create new rss

---
+++ enter_names_urls.py
@@ -0,0 +1,26 @@
+
+with open('soundcloudrss.rss') as old_rss_file:
+    old_rss_list = old_rss_file.readlines()
+    old_rss_list = [name.rstrip('\n') for name in old_rss_list]
+
+with open('episode_links.txt', 'r') as link_file:
+    link_list = link_file.readlines()
+    link_list = [link.rstrip('\n') for link in link_list]
+
+link_list.reverse()
+file_list = [link.replace("open?", "uc?export=download&amp;") for link in link_list]
+
+with open("doppelfokus_rss.rss", "a") as new_rss:
+    file_link = zip(file_list, link_list)
+    for old_rss_line in old_rss_list:
+        if "<link>https://soundcloud.com/doppelfokus" in old_rss_line:
+            file, link = next(file_link)
+            replacement_link = "<link>" + link + "</link>" + "\n"
+            new_rss.write(replacement_link)
+        elif '<enclosure type="audio/mpeg"' in old_rss_line:
+            url = old_rss_line.split("url=")[1]
+            url = url.split(" ")[0]
+            new_line = old_rss_line.replace(url, '"'+file+'"')
+            new_rss.write(new_line+"\n")
+        else:
+            new_rss.write(old_rss_line+"\n")
